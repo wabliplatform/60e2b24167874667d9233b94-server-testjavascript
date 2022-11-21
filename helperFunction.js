7b1c334f2def159942d61010ba833d4cc69239c3
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-22 01:28:29 +0200

    Update helperFunction.js

--- helperFunction.js
+++ helperFunction.js
@@ -5,7 +5,7 @@
 function pregetAllwabli(param) {
   // insert code here
   const f3param = [];
-  let f3param = param.filter(wabli => wabli.wtitle = "1234")
+  f3param = param.filter(wabli => wabli.wtitle = "1234")
   return f3param;
 }
 module.exports = {pregetAllwabli} 
