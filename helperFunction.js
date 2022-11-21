9c807fc1d63e73f40f66781ba9bcd21412c292b2
wabliplatform <93119230+wabliplatform@users.noreply.github.com>
2022-11-22 01:36:28 +0200

    Update helperFunction.js

--- helperFunction.js
+++ helperFunction.js
@@ -7,8 +7,7 @@ function pregetAllwabli(param) {
   const f3param = [];
  
  
- let f3param = param.filter(function(person) {
-	return person.wtitle == "1234")
+ let f3param = param.filter(function(person) {return person.wtitle == "1234"})
  
   return f3param;
 }
